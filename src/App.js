d2166c8c0a151b45fef72504067739288ffc8076
NageswararaoNilla <nillanageswararao075@gmail.com>
2023-09-08 05:44:40 +0000

    functionalities completed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,19 @@
+import {Switch, Route, Redirect} from 'react-router-dom'
+
+import LoginForm from './components/LoginForm'
+import ProtectedRoute from './components/ProtectedRoute'
+import Home from './components/Home'
+import NotFound from './components/NotFound'
+
 import './App.css'
 
-// Replace your code here
-const App = () => <h1>Hello World!</h1>
+const App = () => (
+  <Switch>
+    <Route exact path="/login" component={LoginForm} />
+    <ProtectedRoute exact path="/" component={Home} />
+    <Route path="/not-found" component={NotFound} />
+    <Redirect to="/not-found" />
+  </Switch>
+)
+
+export default App
